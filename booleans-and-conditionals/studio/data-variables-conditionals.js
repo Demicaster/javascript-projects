857df8308afa6186d8e65331d3cc869d08892263
Joseph <Demicaster@gmail.com>
2024-01-11 18:08:35 -0600

    added shuttle checklist

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -13,3 +13,42 @@
 // add logic below to verify the weather status is clear
 
 // Verify shuttle launch can proceed based on above conditions
+
+let date = "Monday 2019-03-18";
+let time = "10:05:34 AM";
+let astronautCount = 7 ;
+let astronautStatus = "ready";
+let averageAstronautMassKg = 80.7 ;
+let crewMassKg = astronautCount * averageAstronautMassKg ;
+let fuelMassKg = 760000 ;
+let shuttleMassKg = 74842.31 ;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg ;
+let maximumMassLimit = 850000 ;
+let fuelTempCelsius = -225 ;
+let minimumFuelTemp = -300 ;
+let maximumFuelTemp = -150 ;
+let fuelLevel = "100%" ;
+let weatherStatus = "clear" ;
+let preparedForLiftOff = true ;
+
+if (astronautCount <= 7)  {
+    console.log("All crew Counted.");
+}
+    if (astronautStatus === "ready") {
+        console.log ("All crew Prepared.") ;
+    }
+        if (totalMassKg < 85000) {
+            console.log ("Weight level stable.") ;
+        }
+        if (fuelTempCelsius > -300 && fuelTempCelsius < -150) {
+                console.log("fuel temperature safe.") ;
+            }
+            if (fuelLevel === "100%") {
+                    console.log("Temperature safe.") ;
+                }
+                if (weatherStatus === "clear")  {
+                    console.log ("shuttle ready for launch!") 
+                    }
+                    else console.log ("Error in launch! Check Weight, Fuel, and Weather.") ;
+
+                        
