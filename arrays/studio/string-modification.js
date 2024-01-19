f7da2cbd8778f2d4b522c868d0cc7d96ad41f9e9
Joseph <Demicaster@gmail.com>
2024-01-18 20:32:07 -0600

    Finished string Modification part still WIP

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -1,6 +1,16 @@
 const input = require('readline-sync');
 let str = "LaunchCode";
 
+let answer = input.question("How piggy should your latin be?");
+if (answer >= 10) {
+    console.log("TO PIGGY!")
+}
+    else{
+    let startString = (str.slice(0,answer)); 
+    let endString = (str.slice(answer,10));
+    console.log(endString + startString );
+    }
+
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
 
